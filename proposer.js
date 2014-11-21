09dc3e8c3f6a8737d9189f47a2f4828e2a82dfe1
vmariano <m.vicent@gmail.com>
2014-11-21 16:48:27 -0300

    add jsdoc

--- proposer.js
+++ proposer.js
@@ -4,17 +4,26 @@ var template = require('./template.html');
 /**
  *  Create a proposer icon.
  *
- * @param {Object} data
- * @param {Object} data
+ * @param {Object} proposer should be set:
+ *
+ * {String} data.imageUrl Url for link image icon
+ * {String} data.image url for the image source jpg| png | etc.
+ * {String} data.authorTitle title for the autor
+ * {String} data.authorUrl url to the author profile/page/external link
+ * {String} data.name Name of the author
+ * {String} data.affiliationTitle Affiliations of the author.
+ * {String} data.affiliationUrl Afiliations of the url.
+ * {String} data.affiliation  name of the author.
+ *
  * @constructor
  */
-function Proposer (data) {
+function Proposer (proposer) {
   if (!this instanceof Proposer) {
-    return new Proposer(data);
+    return new Proposer(proposer);
   }
 
   var View = ripple(template);
-  return new View(data);
+  return new View(proposer);
 }
 
 module.exports = Proposer;
